9fec170cd1e201b4a31044efe827df46d6457712
celebrater <tkgm45nbchk@gmail.com>
2022-03-06 18:14:55 +0900

    added Y direction

--- main.js
+++ main.js
@@ -3,23 +3,36 @@
 const div = document.getElementById("box");
 let startX = null;
 let endX = null;
+let startY = null;
+let endY = null;
 
 const logSwipeStart = (e) => {
     e.preventDefault();
     startX = e.touches[0].pageX;
+    startY = e.touches[0].pageY;
 }
 
-function logSwipe(e) {
+const logSwipe = (e) => {
     e.preventDefault();
     endX = e.touches[0].pageX;
+    endY = e.touches[0].pageY;
 }
 
-function logSwipeEnd(e) {
+const logSwipeEnd = (e) => {
     e.preventDefault();
+    console.log("startX: " + startX);
+    console.log("startY: " + startY);
+    console.log("endX: " + endX);
+    console.log("endY: " + endY);
     if(endX - startX > 0) {
         console.log("Swiped to right.");
     } else {
         console.log("Swiped to left");
+    }
+    if(endY - startY > 0) {
+        console.log("Swiped to bottom");
+    } else {
+        console.log("Swiped to top.");
     }
 }
 
